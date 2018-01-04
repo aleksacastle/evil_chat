f1d655974804260b33759a6e48b9b81cfda4f1a2
aleksacastle <aaklochko@gmail.com>
2018-01-04 19:49:30 +0200

    no more generated files in assets folder

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,13 @@ Bundler.require(*Rails.groups)
 
 module EvilChat
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework  false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.channel         assets: false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
