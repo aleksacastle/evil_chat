d5c4d11af1be3527a0efafa23b45e4ad5541fe94
aleksacastle <aaklochko@gmail.com>
2018-01-05 18:00:52 +0200

    Added react component

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   # Tell controllers where to find partials
-
+  prepend_view_path Rails.root.join("frontend")
 end
