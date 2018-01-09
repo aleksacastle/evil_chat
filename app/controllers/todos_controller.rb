d17ef67c3190b0824488fe212e6455b8ec2c48c3
aleksacastle <aaklochko@gmail.com>
2018-01-09 16:12:57 +0200

    added todo controller

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,41 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @todos }
+    end
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    respond_to do |format|
+      format.json do
+        if @todo.save
+          render :json => @todo
+        else
+          render :json => { :errors => @todo.errors.messages }, :status => 422
+        end
+      end
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    respond_to do |format|
+      format.json do
+        if @todo.update(todo_params)
+          render :json => @todo
+        else
+          render :json => { :errors => @todo.errors.messages }, :status => 422
+        end
+      end
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :status)
+  end
+end
