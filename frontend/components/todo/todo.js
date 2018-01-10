0dd1fb536222520e41e3ee393f39438e92bd2f33
aleksacastle <aaklochko@gmail.com>
2018-01-10 13:36:55 +0200

    added todo add func without rails

--- frontend/components/todo/todo.js
+++ frontend/components/todo/todo.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./todo.css";
 import TodoList from "./todoList";
 
 var destination = document.querySelector("#container");
 
 ReactDOM.render(
-    <div>
-      <h1>React todo list</h1>
-      <TodoList/>
-    </div>,
-    destination
+  <div>
+    <h1>React todo list</h1>
+    <TodoList />
+  </div>,
+  destination
 );
