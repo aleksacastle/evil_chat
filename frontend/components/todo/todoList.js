e5a923e190ff15e87b8853c62cf50c6c38291d96
aleksacastle <aaklochko@gmail.com>
2018-01-10 13:54:33 +0200

    added delete todo function

--- frontend/components/todo/todoList.js
+++ frontend/components/todo/todoList.js
@@ -10,6 +10,7 @@ class TodoList extends Component {
      };
 
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
   addItem(e) {
 
@@ -32,6 +33,17 @@ class TodoList extends Component {
 
       e.preventDefault();
   }
+
+  deleteItem(key) {
+  var filteredItems = this.state.items.filter(function (item) {
+    return (item.key !== key);
+  });
+
+  this.setState({
+    items: filteredItems
+  });
+}
+
   render() {
     return (
       <div className="todoListMain">
@@ -43,7 +55,8 @@ class TodoList extends Component {
             <button type="submit">add</button>
           </form>
         </div>
-        <TodoItems entries={this.state.items}/>
+        <TodoItems entries={this.state.items}
+                  delete={this.deleteItem}/>
       </div>
     );
   }
